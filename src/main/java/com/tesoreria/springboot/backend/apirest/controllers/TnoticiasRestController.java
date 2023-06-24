b4efb33bdbdbaefc920bb79b9dcfcb94a645eb3c
Hiram weyman <41969577+HiramWeyman@users.noreply.github.com>
2023-06-24 14:30:10 -0600

    Update TnoticiasRestController.java

--- src/main/java/com/tesoreria/springboot/backend/apirest/controllers/TnoticiasRestController.java
+++ src/main/java/com/tesoreria/springboot/backend/apirest/controllers/TnoticiasRestController.java
@@ -17,7 +17,7 @@ import com.tesoreria.springboot.backend.apirest.models.dao.ITnoticiasDao;
 import com.tesoreria.springboot.backend.apirest.models.entity.Tnoticias;
 import com.tesoreria.springboot.backend.apirest.models.services.ITnoticiasService;
 
-//@CrossOrigin(origins = { "http://localhost:4200", "http://192.168.1.190:8080" })
+
 @CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
