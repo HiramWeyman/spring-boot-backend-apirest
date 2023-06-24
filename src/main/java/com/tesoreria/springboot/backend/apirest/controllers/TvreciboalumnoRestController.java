cef25245cfaa6ed776df4df849d09c1820f60140
Hiram weyman <41969577+HiramWeyman@users.noreply.github.com>
2023-06-24 14:37:08 -0600

    Update TvreciboalumnoRestController.java

--- src/main/java/com/tesoreria/springboot/backend/apirest/controllers/TvreciboalumnoRestController.java
+++ src/main/java/com/tesoreria/springboot/backend/apirest/controllers/TvreciboalumnoRestController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tesoreria.springboot.backend.apirest.models.dao.ITvreciboalumnoDao;
 import com.tesoreria.springboot.backend.apirest.models.entity.Tvreciboalumno;
 
-//@CrossOrigin(origins = { "http://localhost:4200", "http://192.168.1.190:8080" })
+
 @CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
