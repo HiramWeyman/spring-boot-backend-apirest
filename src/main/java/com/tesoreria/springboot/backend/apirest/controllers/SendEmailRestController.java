78b267dcaa2f7025c79765d0b3d3ca189bfc8861
Hiram weyman <41969577+HiramWeyman@users.noreply.github.com>
2023-06-24 14:27:34 -0600

    Update SendEmailRestController.java

--- src/main/java/com/tesoreria/springboot/backend/apirest/controllers/SendEmailRestController.java
+++ src/main/java/com/tesoreria/springboot/backend/apirest/controllers/SendEmailRestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@CrossOrigin(origins = { "http://localhost:4200", "http://192.168.1.190:8080" })
+
 @CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
