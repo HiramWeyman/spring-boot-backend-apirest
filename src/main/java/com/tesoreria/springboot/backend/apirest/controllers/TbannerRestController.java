5ab9607312f234821a7f9cb3317975708a2995dd
Hiram weyman <41969577+HiramWeyman@users.noreply.github.com>
2023-06-24 14:28:06 -0600

    Update TbannerRestController.java

--- src/main/java/com/tesoreria/springboot/backend/apirest/controllers/TbannerRestController.java
+++ src/main/java/com/tesoreria/springboot/backend/apirest/controllers/TbannerRestController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tesoreria.springboot.backend.apirest.models.dao.ITbannerDao;
 import com.tesoreria.springboot.backend.apirest.models.entity.Tbanner;
 
-//@CrossOrigin(origins = { "http://localhost:4200", "http://192.168.1.190:8080" })
+
 @CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
