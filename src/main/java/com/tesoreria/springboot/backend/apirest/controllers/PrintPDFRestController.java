8d8ab9062f6000a42632f298813f94c178387954
Hiram weyman <41969577+HiramWeyman@users.noreply.github.com>
2023-06-24 14:26:16 -0600

    Update PrintPDFRestController.java

--- src/main/java/com/tesoreria/springboot/backend/apirest/controllers/PrintPDFRestController.java
+++ src/main/java/com/tesoreria/springboot/backend/apirest/controllers/PrintPDFRestController.java
@@ -98,7 +98,7 @@ public class PrintPDFRestController {
 		try {
             Class.forName("oracle.jdbc.OracleDriver");
         
-		conn = DriverManager.getConnection("jdbc:oracle:thin:@192.168.1.184:1527:sai2", "TESOPORTAL", "TP1968");
+		conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1528:sai3", "", "");
         if (conn != null)
             System.out.println("Database Connected");
         else
@@ -161,14 +161,10 @@ public class PrintPDFRestController {
 	@GetMapping(value = "/printDesc/{id}/{ref_banco}")
 	public void reportDesc(HttpServletResponse response, @PathVariable("id") String id,@PathVariable("ref_banco") String ref_banco) throws Exception {
 		
-		//String Url = "https://evopaymentsmexico.gateway.mastercard.com/api/rest/version/52/merchant/TEST2017ECOMM1/order/"+ref_banco+"";
-		//String Url = "https://evopaymentsmexico.gateway.mastercard.com/api/rest/version/52/merchant/TEST1143891/order/"+ref_banco+"";
-		String Url = "https://evopaymentsmexico.gateway.mastercard.com/api/rest/version/52/merchant/1143891/order/"+ref_banco+"";
+		String Url = "https://evopaymentsmexico.gateway.mastercard.com/api/rest/version/52/merchant/TEST2017ECOMM1/order/"+ref_banco+"";
 		URL url = new URL(Url);
 		URLConnection uc = url.openConnection();
-		//String userpass = "merchant.TEST2017ECOMM1" + ":" + "ac50e16a0c295b971ed55b0086ad0262";
-		//String userpass = "merchant.TEST1143891" + ":" + "2d6535bb8bff89759b283e56823a8ad5";
-		String userpass = "merchant.1143891" + ":" + "8d56e8d4b2753f1ac9286a70ec23f779";
+		String userpass = "merchant.TEST2017ECOMM1" + ":" + "ac50e16a0c295b971ed55b0086ad0262";
 		String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userpass.getBytes()));
 		uc.setRequestProperty ("Authorization", basicAuth);
 		
@@ -211,7 +207,7 @@ public class PrintPDFRestController {
 		try {
             Class.forName("oracle.jdbc.OracleDriver");
         
-		conn = DriverManager.getConnection("jdbc:oracle:thin:@192.168.1.184:1527:sai2", "TESOPORTAL", "TP1968");
+		conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1528:sai3", "", "");
         if (conn != null)
             System.out.println("Database Connected");
         else
